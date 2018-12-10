48a6ee8007b3e09d06f70127725e875bb19bf056
Travis <travissmith1998@gmail.com>
2018-12-10 02:16:39 -0600

    code re-fractoring, added HR support but still need to work out bugs with checking multiple flipped replays while flipped

--- ReplaySBS/old.py
+++ ReplaySBS/old.py
@@ -1,3 +1,7 @@
+
+
+
+
 from osrparse import parse_replay_file
 import os
 from os import listdir
@@ -6,6 +10,11 @@ import difflib
 import math
 import json
 
+#This program compares a user's replay with others in order to see how close the cursor movement is to eachother
+#The test replays here are Cookiezi's Timefreeze  FC, _Ryuk's FC, and a replay botted copy of Cookiezi's FC
+#The distances of the x and y coordinates of each play are compared against each-other
+#The average distance of the cursor between the legit and copied Cookiezi play is about 13 pixels
+#The average distance of the cursor between the legit _Ryuk play and Cookiezi play is about 156 pixels
 
 pathToUserReplay = "C:\\Users\\Travis\\source\\repos\\ReplaySBS\\ReplaySBS\\replays\\userReplay\\"
 
@@ -15,66 +24,68 @@ pathToOtherReplays = "C:\\Users\\Travis\\source\\repos\\ReplaySBS\\ReplaySBS\\re
 averageDistances = [] #Stores the average distances between the user's replay and the all the ones it was checked against
 flippedDistances = [] #used to store the distances of flipped coordinates of user's replays
 
-userCorrds = [] # Where the coordinates of the user's replay will be stored
+with open("apikeys.json") as apikeys:
+    keys = json.load(apikeys)
 
+key = keys['osu_apikey']
+mainApi = 'https://osu.ppy.sh/api/'
 
-
-def parseUserReplay():
-    # Parse user replay
+    
+def checkDiffInReplays():
+    
     
     userOsrList = [f for f in listdir(pathToUserReplay) if isfile(join(pathToUserReplay, f))]
     
+    userCorrds = [] # Where the coordinates of the user's replay will be stored
+    otherCoords = [] # Where the coordinates of the other replays will be stored
+
+    
+    # Parse user replay
     for userOsr in userOsrList: #For every user replay (should only be one for now)
         userOsr = pathToUserReplay + userOsr
-       
+        print("User Osr: " + userOsr)
         userReplay = parse_replay_file(userOsr)
         playData = userReplay.play_data
-        userPlayerName = userReplay.player_name
-        print("User's Name: " + userPlayerName)
-        #print("")
         
         for play in playData:
              userCorrds.append((play.x, play.y))
-
-    return userPlayerName
-
-
-def parseOtherReplays(userPlayerName):
-    #parse other replays
+            
+        parseOtherReplays(userCorrds) #parses the other replay, then checks for similarity
+        
     
+def parseOtherReplays(usersCoords): # Parse other replays
     osrList = [f for f in listdir(pathToOtherReplays) if isfile(join(pathToOtherReplays, f))]
-    
+    #print(osrList)
     for osr in osrList:
         
         otherCoords = []
         
+        print("Replay Osr: " + osr)
+        print("")
         osr = pathToOtherReplays + osr
         
         replay = parse_replay_file(osr)
         playData = replay.play_data
-        otherPlayerName = replay.player_name
-        
-        #print("")
-        #print("Other Player: " + otherPlayerName)
-        
         
         for play in playData:
             otherCoords.append((play.x, play.y))
-
-        #averageDistances.append(userPlayerName + "" +otherPlayerName)
-        averageDistance = (computeSimilarity(userCorrds, otherCoords, userPlayerName, otherPlayerName))
+    
+        averageDistance = (computeSimilarity(usersCoords, otherCoords))
         
-
-    return otherPlayerName
-
-
-
-def computeSimilarity(userCoords, otherCoords, userPlayerName, otherPlayerName): # Calculates distance between the cursor between the users replay and the other replay
+        averageDistances.append((averageDistance, osr)) #appends the osr filename and the average distance 
+        
+    
+        print("Average distance is " +str(averageDistance))
+        print("")
+    
+    return 0
+            
+            
+def computeSimilarity(userCoords, otherCoords ): # Calculates distance between the cursor between the users replay and the other replay
    
     distances = []
     totalDistance = 0
     totalFlippedDistance = 0
-    players= userPlayerName + " vs " +otherPlayerName
     
     allCoords = list(zip(userCoords, otherCoords)) # Combines the replay coordinates from both replays into one list so we can iterate through it at the same time
 
@@ -100,7 +111,7 @@ def computeSimilarity(userCoords, otherCoords, userPlayerName, otherPlayerName):
         y2 = allCoords[i][0][1] # user coords y
         y1 = allCoords[i][1][1] # other coords y
         
-        flippedY = (192-y2 ) + 192 #converts the replay's y value to the HR y value
+        flippedY = (190-y2 ) + 190 #converts the replay's y value to the HR y value
         
         #print("")
         #print("userFlipped " + str(x2), str(flippedY))
@@ -131,31 +142,31 @@ def computeSimilarity(userCoords, otherCoords, userPlayerName, otherPlayerName):
     averageDistance = (totalDistance/len(distances))
     averageFlippedDistance = (totalFlippedDistance/len(distances))
     
-    #print("user replay vs other replay " +str(averageDistance))
+    print("user replay vs other replay " +str(averageDistance))
    
-    #print("flipped user replay vs other replay " +str(averageFlippedDistance))
+    print("flipped user replay vs other replay " +str(averageFlippedDistance))
 
     if averageDistance < averageFlippedDistance:
-        averageDistances.append(str(averageDistance) + " " + players)
-        
+        return averageDistance
     else:
-        averageDistances.append(str(averageFlippedDistance) + " " + players)
-
-
+        return averageFlippedDistance
     
-        
-    print("")
-    print("Similarity of " +userPlayerName + " and " +otherPlayerName + " is " +str(averageDistance))
 
-
-def checkDiffInReplays():
-    userPlayerName = parseUserReplay()
-    parseOtherReplays(userPlayerName)
-   
-    
  
-checkDiffInReplays()
+        
+    
 
+
+def getReplay():
+    getReplay = 'get_replay?' +key
+    
+    
+
+
+
+
+
+checkDiffInReplays()
 
 
 #Summary of findings, needs to be cleaned up
