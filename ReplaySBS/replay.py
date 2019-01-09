cfc55279f06b5fe8125571fab0529e0676ff89a2
Samuel Klumpers <samuelhklumpers@gmail.com>
2019-01-10 00:08:34 +0100

    unfinished commit

--- ReplaySBS/replay.py
+++ ReplaySBS/replay.py
@@ -2,6 +2,8 @@ import math
 
 from config import REPLAY_PORTION
 
+import numpy as np
+
 class Replay:
     def __init__(self, replay_data):
         self.player_name = replay_data.player_name
@@ -10,35 +12,19 @@ class Replay:
         
     @staticmethod
     def compute_similarity(user_replay, check_replay):
-        coords_user = []
-        coords_check = []
-
-
         players = " ({} vs {})".format(user_replay.player_name, check_replay.player_name)
         
-        user_replay_data = user_replay.play_data
-        check_replay_data = check_replay.play_data
+        get_xy = lambda event: (event.x, event.y)
+        
+        coords_user = np.array(list(map(get_xy, user_replay.play_data)))
+        coords_check = np.array(list(map(get_xy, check_replay.play_data)))
 
-        for data in user_replay_data:
-            coords_user.append((data.x, data.y))
-        for data in check_replay_data:
-            coords_check.append((data.x, data.y))
+        difference = coords_user - coords_check
 
-        coords = list(zip(coords_user, coords_check))
+        distance = (difference ** 2).sum(axis = 1) ** 0.5
 
-        distance_total = 0
-        length = int(len(coords) * REPLAY_PORTION)
-        for user, check in coords[0:length]:
-            x1 = user[0]
-            x2 = check[0]
-            y1 = user[1]
-            y2 = check[1]
-
-            distance = math.sqrt((x2-x1)**2 + (y2-y1)**2)
-            distance_total += distance
-
-            
-        distance_average = (distance_total/length)
-
-        return str(distance_average) + players
+        mu = distance.mean()
+        sigma = distance.std()
+        
+        return str(mu) + ", " + str(sigma) + players
  
