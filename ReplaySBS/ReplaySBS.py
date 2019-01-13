45764c8c7b0cb3daf097b2d6492749343fbbce88
tybug <orionldevoe@gmail.com>
2019-01-13 12:23:32 -0500

    implement checking an entire map leaderboard

--- ReplaySBS/ReplaySBS.py
+++ ReplaySBS/ReplaySBS.py
@@ -1,5 +1,6 @@
 from argparser import argparser
 import requests
+import itertools
 
 from downloader import Downloader
 from replay import Replay
@@ -9,15 +10,23 @@ def main():
     """checks all replays in PATH_REPLAYS_USER against all replays in PATH_REPLAYS_CHECK"""
 
     args = argparser.parse_args()
-    if(args.map_id):
-        if(args.user_id):
-            user_replay = Replay.from_map(args.map_id, args.user_id, args.user_id)
-        else:
-            user_replay = Replay.from_path(PATH_REPLAYS_USER[0])
-        
+    if(args.map_id and args.user_id): # passed both -m and -u
+        user_replay = Replay.from_map(args.map_id, args.user_id, args.user_id)
+
         for check_id in Downloader.users_from_beatmap(args.map_id):
             check_replay = Replay.from_map(args.map_id, check_id, check_id)
             print(Replay.compute_similarity(user_replay, check_replay))
+        
+        return
+
+    if(args.map_id): # only passed -m
+        # get all 50 top replays
+        replays = [Replay.from_map(args.map_id, check_id, check_id) for check_id in Downloader.users_from_beatmap(args.map_id)]
+        for replay1, replay2 in itertools.combinations(replays, 2):
+            print(Replay.compute_similarity(replay1, replay2))
+       
+        return
+        
 
     # checks every replay listed in PATH_REPLAYS_USER against every replay listed in PATH_REPLAYS_CHECK
     for osr_path in PATH_REPLAYS_USER:
