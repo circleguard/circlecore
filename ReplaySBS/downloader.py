c08710e6b7923e4ce15756a9961a8f0493041dc0
tybug <orionldevoe@gmail.com>
2019-01-13 09:52:39 -0500

    make downloader a class with only static methods

--- ReplaySBS/downloader.py
+++ ReplaySBS/downloader.py
@@ -4,28 +4,39 @@ import secret
 from config import API_SCORES, API_REPLAY
 
 
-"""
-This module is an attempt at the singelton pattern, with one instance used throughout the project.
-
-Manages interactions with the osu api - keeps track of 
-ratelimits for different types of requests, and self-ratelimits by sleeping the thread.
-"""
-
-
-def users_from_beatmap(map_id):
+class Downloader():
     """
-    Returns a list of all user ids of the top 50 plays on the given beatmap.
+    This class is not meant to be instantiated, instead only static methods and class variables used.
+    This is because we only use one api key for the entire project, and making all methods static provides
+    cleaner access than passing around a single Downloader class.
+
+    Manages interactions with the osu api - keeps track of 
+    ratelimits for different types of requests, and self-ratelimits by sleeping the thread.
     """
 
-    url = API_SCORES.format(map_id)
-    return [x["user_id"] for x in requests.get(url).json()]
+    def __init__(self):
+        """
+        This class should never be instantiated. All methods are static.
+        """
 
-def replay_data(map_id, user_id):
-    """
-    Queries the api for replay data from the given user on the given map.
+        raise Exception("This class is not meant to be instantiated. Use the static methods instead")
 
-    Returns:
-        The lzma bytestring returned by the api.
-    """
+    @staticmethod
+    def users_from_beatmap(map_id):
+        """
+        Returns a list of all user ids of the top 50 plays on the given beatmap.
+        """
 
-    return requests.get(API_REPLAY.format(map_id, user_id)).json()["content"]
+        url = API_SCORES.format(map_id)
+        return [x["user_id"] for x in requests.get(url).json()]
+
+    @staticmethod
+    def replay_data(map_id, user_id):
+        """
+        Queries the api for replay data from the given user on the given map.
+
+        Returns:
+            The lzma bytestring returned by the api.
+        """
+
+        return requests.get(API_REPLAY.format(map_id, user_id)).json()["content"]
