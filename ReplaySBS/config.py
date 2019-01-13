ea014e2f4752556175fe8058d96d9a65cbc9cae3
tybug <orionldevoe@gmail.com>
2019-01-13 01:23:19 -0500

    compare every replay regardless of name

--- ReplaySBS/config.py
+++ ReplaySBS/config.py
@@ -8,9 +8,10 @@ from secret import API_KEY
 PATH_REPLAYS = pathlib.Path(__file__).parent / "replays"
 
 # names of replays to check
-PATH_REPLAYS_USER = [join(PATH_REPLAYS, "user", path) for path in ["cookiezi_undead.osr"]]
+PATH_REPLAYS_USER_STUB = join(PATH_REPLAYS, "user")
 PATH_REPLAYS_CHECK_STUB = join(PATH_REPLAYS, "compare") # path of replays to check against
 
+PATH_REPLAYS_USER = [join(PATH_REPLAYS_USER_STUB, f) for f in os.listdir(PATH_REPLAYS_USER_STUB) if isfile(join(PATH_REPLAYS_USER_STUB, f)) and f != ".DS_Store"]
 # get all replays in path to check against
 PATH_REPLAYS_CHECK = [join(PATH_REPLAYS_CHECK_STUB, f) for f in os.listdir(PATH_REPLAYS_CHECK_STUB) if isfile(join(PATH_REPLAYS_CHECK_STUB, f)) and f != ".DS_Store"]
 
