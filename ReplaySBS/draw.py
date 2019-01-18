960707867494b9498ab19d9eb946fc4be831914e
tybug <orionldevoe@gmail.com>
2019-01-18 15:12:29 -0500

    use period at end of docstrings

--- ReplaySBS/draw.py
+++ ReplaySBS/draw.py
@@ -14,8 +14,8 @@ class Draw():
         Show an animation of both replays overlayed on top of eachother.
 
         Args:
-            Replay user_replay: The first Replay to draw
-            Replay check_replay: The second Replay to draw
+            Replay user_replay: The first Replay to draw.
+            Replay check_replay: The second Replay to draw.
         """
 
         data1 = user_replay.as_list_with_timestamps()
