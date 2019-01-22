150ebaf4a2fc6e9954f0f9723c906a5548896350
tybug <orionldevoe@gmail.com>
2019-01-21 22:07:02 -0500

    fix relpay creation to parse lzma string first

--- osu-ac/replay.py
+++ osu-ac/replay.py
@@ -11,18 +11,21 @@ from cacher import Cacher
 def check_cache(function):
     """
     Decorator that checks if the replay by the given user_id on the given map_id is already cached.
-    If so, returns the cached string instead of requesting it from the api.
+    If so, returns a Replay instance from the cached string instead of requesting it from the api.
 
     Note that map_id and user_id must be the first and second arguments to the function respectively.
+
+    Returns:
+        A Replay instance from the cached replay if it was cached, or the return value of the function if not.
     """
 
     def wrapper(*args, **kwargs):
         map_id = args[0]
         user_id = args[1]
-        result = Cacher.check_cache(map_id, user_id)
-        if(result):
-            print("replay cached")
-            return result
+        lzma = Cacher.check_cache(map_id, user_id)
+        if(lzma):
+            replay_data = osrparse.parse_replay(lzma, pure_lzma=True).play_data
+            return Replay(replay_data, user_id)
         else:
             function(*args, **kwargs)
     return wrapper
