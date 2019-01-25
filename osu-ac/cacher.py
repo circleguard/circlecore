6a869530466fc0e81b3c4fd2443f08319f2fd1b2
ChrisMiuchiz <chris000005@gmail.com>
2019-01-24 21:44:59 -0500

    move stuff out of cacher and into wtc

--- osu-ac/cacher.py
+++ osu-ac/cacher.py
@@ -2,8 +2,7 @@ import sqlite3
 
 from config import PATH_DB
 
-import struct
-import lzma
+import wtc
 
 class Cacher:
     """
@@ -36,7 +35,7 @@ class Cacher:
         """
 
         compressed_string = Cacher.compress(lzma_string)
-        packed_osr = Cacher.pack_osr(compressed_string)
+        packed_osr = wtc.compress(compressed_string)
         Cacher.write("INSERT INTO replays VALUES(?, ?, ?, ?)", [map_id, user_id, packed_osr, replay_id])
 
     @staticmethod
@@ -65,7 +64,7 @@ class Cacher:
         """
 
         result = Cacher.cursor.execute("SELECT replay_data FROM replays WHERE map_id=? AND user_id=?", [map_id, user_id]).fetchone()
-        return Cacher.unpack_osr(result[0]) if result else None
+        return wtc.decompress(result[0]) if result else None
 
 
 
@@ -94,122 +93,3 @@ class Cacher:
             A compressed bytestring from the given bytestring
         """
         return lzma_string
-
-    @staticmethod
-    def __PackInt24(integer):
-        """
-        Converts an integer to a 24 bit bytes object.
-
-        Args:
-            int integer: The number to be converted
-
-        Returns:
-            A 24 bit int as bytes
-        """
-        if not (-0x800000 <= integer < 0x800000):
-            raise ValueError('Value must be between -0x800000 and 0x800000')
-        output = struct.pack('<i', integer)
-        output = output[:-1]
-        return output
-
-    @staticmethod
-    def __UnpackInt24(intbytes):
-        """
-        Converts a 24 bit bytes object to an integer.
-
-        Args:
-            bytes intbytes: The bytes to convert to int
-
-        Returns:
-            An integer representation of the input
-        """
-        if len(intbytes) != 3:
-            raise ValueError('Value must be an int24')
-        sign = intbytes[-1] & 0x80
-        if sign:
-            intbytes = intbytes + b'\xFF'
-        else:
-            intbytes = intbytes + b'\x00'
-        return struct.unpack('<i', intbytes)[0]
-
-    @staticmethod
-    def pack_osr(lzma_stream):
-        """
-        Packs replay into a more compact format
-
-        Args:
-            bytes lzma_stream: lzma stream from a replay
-
-        Returns:
-            An lzma compressed bytestring
-        """
-        text = lzma.decompress(lzma_stream).decode('UTF-8')
-        raw = b''
-        for frame in text.split(','):
-            if not frame:
-                continue
-            w, x, y, z = frame.split('|')
-            w = int(w)
-            x = float(x)
-            y = float(y)
-            z = int(z)
-
-            #Everything we need from Z is in the first byte
-            z = z & 0xFF
-
-            #To fit x and y into shorts, they can be scaled to retain more precision.
-            x = int(round(x * 16))
-            y = int(round(y * 16))
-
-
-            #Prevent the coordinates from being too large for a short. If this happens, the cursor is way offscreen anyway.
-            if x <= -0x8000: x = -0x8000
-            elif x >= 0x7FFF: x = 0x7FFF
-            if y <= -0x8000: y = -0x8000
-            elif y >= 0x7FFF: y = 0x7FFF
-            
-
-            #w: signed 24bit integer
-            #x: signed short
-            #y: signed short
-            #z: unsigned char
-            raw += Cacher.__PackInt24(w) + struct.pack('<hhB', x, y, z)
-
-            
-        compressed = lzma.compress(raw, format=2)
-        return compressed
-
-    @staticmethod
-    def unpack_osr(encoded_data):
-        """
-        Unpacks replay data into the more familiar replay format
-
-        Args:
-            bytes encoded_data: Packed replay data from pack_osr
-
-        Returns:
-            An lzma compressed bytestring (Just like the one used in normal OSRs)
-        """
-        output = ''
-        data = lzma.decompress(encoded_data)
-        #each frame is 8 bytes
-        for i in range(0, len(data), 8):
-            frame = data[i : i+8]
-            #extract W on its own since it's an int24 and cannot be used with struct.unpack
-            b_w, frame = frame[:3], frame[3:]
-            
-            #w: signed 24bit integer
-            w = Cacher.__UnpackInt24(b_w)
-
-            #x: signed short
-            #y: signed short
-            #z: unsigned char
-            x, y, z = struct.unpack('<hhB', frame)
-
-            #X and Y are stored as shorts; convert and scale them back to their float forms
-            x /= 16
-            y /= 16
-            
-            output += f'{w}|{x}|{y}|{z},'
-        lzma_stream = lzma.compress(output.encode('UTF-8'), format=2)
-        return lzma_stream
