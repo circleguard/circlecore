3469e28402d935fce194110f03d1553ba49ea507
Samuel Klumpers <samuelhklumpers@gmail.com>
2019-02-10 14:16:49 +0100

    create Screener class

---
+++ circleguard/screener.py
@@ -0,0 +1,32 @@
+class Screener:
+    """
+    A class for screening a single player's history.
+
+    Attributes:
+        Loader loader: The loader all api requests are handled with.
+        Integer user_id: The user to screen.
+        Integer threshold: See Comparer
+        Boolean silent: See Comparer
+        Float stddev: See Comparer
+
+    See Also:
+        Comparer
+    """
+    
+    def __init__(self, loader, user_id, threshold, silent, stddev=None):
+        """
+        Initializes a Screener instance.
+
+        Args:
+            Loader loader: The loader all api requests are handled with.
+            Integer user_id: The user to screen.
+            Integer threshold: See Comparer
+            Boolean silent: See Comparer
+            Float stddev: See Comparer
+        """
+        
+        self.loader = loader
+        self.user_id = user_id
+        self.threshold = threshold
+        self.silent = silent
+        self.stddev = stddev
