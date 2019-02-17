f51a81c16165d907532172e37ffcd345aa740c41
tybug <orionldevoe@gmail.com>
2019-02-17 16:44:07 -0500

    separate stealing and remodding checks

--- circleguard/screener.py
+++ circleguard/screener.py
@@ -37,48 +37,66 @@ class Screener:
         self.number = number
         self.stddevs = stddevs
 
+        best = self.loader.get_user_best(self.user_id, self.number)
+        self.map_ids = [play['beatmap_id'] for play in best]
+
     def screen(self):
         """
         Starts the screening.
         """
 
+        print("Screening user {}".format(self.user_id))
+        self._screen_steal()
+        self._screen_remodding()
+        print("Finished screening.")
 
-        print(f"Screening user {self.user_id}. Replays may appear to be downloaded multiple times for some maps - this is to check for play remodding.")
 
-        best = self.loader.get_user_best(self.user_id, self.number)
-        map_ids = [play['beatmap_id'] for play in best]
-        for i, map_id in enumerate(map_ids, 1):
+    def _screen_steal(self):
+        """
+        Checks the top plays of the user for replay stealing
+        """
 
-            print(f"Screening on map {map_id} (map {i}/{len(best)})")
+        print("checking for replay stealing")
 
+        for i, map_id in enumerate(self.map_ids, 1):
+
+            print(f"Screening on map {map_id} (map {i}/{len(self.map_ids)})")
+            self.loader.new_session(len(self.number))
             # load screened player
-            user_info = self.loader.user_info(map_id, self.user_id, limit=False)
+            user_info = self.loader.user_info(map_id, self.user_id)
 
-            if(user_info[0][3] == 0): # first replay unavailable (so all subsequent ones are as well), check before we waste a heavy api call
+
+            if(user_info[0][3] == 0): # replay unavailable, check before we waste a heavy api call
                 print("replay unavailable for screened user, skipping map {}".format(map_id))
                 continue
 
-            all_replays = self.loader.replay_from_user_info(self.cacher, map_id, user_info)
+            replay1 = self.loader.replay_from_user_info(self.cacher, map_id, user_info)
              # TODO move "replay unavailable so don't load" logic to replay_from_user_info, will mean standardizing
              # loader#users_info to return replay available as well
 
             # load other players on map
             other_users_info = self.loader.users_info(map_id, self.number)
-            # filter out their own replay (happens if they're in the top self.number of that beatmap)
+            # filter out screened user's own replay (happens if they're in the top self.number of that beatmap)
             other_users_info = [info for info in other_users_info if info[0] != self.user_id]
 
             replays2 = self.loader.replay_from_user_info(self.cacher, map_id, other_users_info)
 
             # only compare the first replay for replay stealing, highly unlikely they would steal a lower placed replay
             # TODO make a deep investigate compare all?
-            comparer = Comparer(self.threshold, self.silent, [all_replays[0]], replays2=replays2, stddevs=self.stddevs)
+            comparer = Comparer(self.threshold, self.silent, replay1, replays2=replays2, stddevs=self.stddevs)
             comparer.compare(mode="double")
 
-            print("checking for remodding")
-            # now compare all their replays against each other for replay remodding
-            comparer = Comparer(self.threshold, self.silent, all_replays, stddevs=self.stddevs)
+    def _screen_remodding(self):
+        """
+        Checks the top plays of the user for remodding
+        (taking a replay and resubmitting it with different mods, typically for more pp or score)
+        """
+
+        print("checking for remodding. The same replay may appear to be downloaded multiple times for some maps - don't worry, they have different mods.")
+        for i, map_id in enumerate(self.map_ids, 1):
+            print(f"Screening on map {map_id} (map {i}/{len(self.map_ids)})")
+            user_info = self.loader.user_info(map_id, self.user_id, limit=False)
+            self.loader.new_session(len(user_info))
+            replays1 = self.loader.replay_from_user_info(self.cacher, map_id, user_info)
+            comparer = Comparer(self.threshold, self.silent, replays1, stddevs=self.stddevs)
             comparer.compare(mode="single")
-
-            self.loader.reset()
-
-        print("Finished screening")
