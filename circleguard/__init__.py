409873cb071ac3f750cfed9ed6a77ade8d7c98e1
tybug <orionldevoe@gmail.com>
2019-04-25 10:51:02 -0400

    use explicit imports in __init__

--- circleguard/__init__.py
+++ circleguard/__init__.py
@@ -1,5 +1,5 @@
-from .circleguard import Circleguard, set_options
-from .replay import *
+from circleguard.circleguard import Circleguard, set_options
+from circleguard.replay import Check, Replay, ReplayMap, ReplayPath
 from circleguard.detect import Detect
 
 __version__ = "2.0d"
