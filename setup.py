0429f54fb1bfd4efb438b30d739b6a7764eb5676
tybug <orionldevoe@gmail.com>
2019-07-24 12:35:06 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -1,25 +1,31 @@
-import setuptools
+from distutils.core import setup
+from setuptools import find_packages
+from circleguard.__init__ import __version__
 
 with open("README.md", "r") as readme:
     long_description = readme.read()
 
-setuptools.setup(
+setup(
     name="circleguard",
-    version="2.0",
-    author="Liam DeVoe, Samuel",
-    author_email="orionldevoe@gmail.com",
+    version=__version__,
     description="A player made and maintained cheat detection tool for osu!. "
                 "Provides support for detecting replay stealing and remodding "
                 "from a profile, map, or set of osr files.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/circleguard/circlecore",
-    packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
+        "Intended Audience :: Developers",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    keywords = ["osu!, python, cheat-detection, replay-stealing, remodding"],
+    author="Liam DeVoe",
+    author_email="orionldevoe@gmail.com",
+    url="https://github.com/circleguard/circlecore",
+    download_url = "https://github.com/circleguard/circlecore/tarball/" + __version__,
+    license = "MIT",
+    packages=find_packages(),
     install_requires=[
         "circleparse",
         "numpy",
