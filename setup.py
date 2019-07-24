020c30c78fe51e48584e409fd06dbf2b1a25bdb6
tybug <orionldevoe@gmail.com>
2019-07-24 12:17:38 -0400

    update setup.py

--- setup.py
+++ setup.py
@@ -6,9 +6,11 @@ with open("README.md", "r") as readme:
 setuptools.setup(
     name="circleguard",
     version="2.0",
-    author="Liam DeVoe, Travis Smith, Samuel xx",
-    author_email="orionldevoe@gmail.com, xxxx, xxxx",
-    description="Detect cheaters from replay files, leaderboards, or profiles",
+    author="Liam DeVoe, Samuel",
+    author_email="orionldevoe@gmail.com",
+    description="A player made and maintained cheat detection tool for osu!. "
+                "Provides support for detecting replay stealing and remodding "
+                "from a profile, map, or set of osr files.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/circleguard/circlecore",
@@ -18,4 +20,11 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    install_requires=[
+        "circleparse",
+        "numpy",
+        "wtc",
+        "requests",
+        "ossapi"
+    ]
 )
