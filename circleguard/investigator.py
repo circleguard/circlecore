5c21ed83388cacbc4bda7d12d85a7ccb69420ac1
InvisibleSymbol <pettypalkovicsreal@gmail.com>
2019-08-16 18:56:26 +0200

    split filter from ur

--- circleguard/investigator.py
+++ circleguard/investigator.py
@@ -15,25 +15,11 @@ class Investigator:
     def ur(self):
         hitobjs = self._parse_beatmap(self.beatmap)
         keypresses = self._parse_keys(self.replay)
-        hitwindow = 150 + 50 * (5 - self.beatmap.difficulty["OverallDifficulty"]) / 5
+        filtered_array = self._filter_hits(hitobjs, keypresses)
         diff_array = []
-
-        # inefficient as fugg, but not important rn
-        # filters out all clicks that didn't hit anything and stores diffs to array
-        for hitobj in hitobjs:
-            temp_diffs = []
-            temp_times = []
-            for press in keypresses:
-                if hitobj > press-(hitwindow/2) and hitobj < press+(hitwindow/2):
-                    temp_times.append(hitobj)
-                    diff = press-hitobj
-                    diff = diff if diff > 0 else diff
-                    temp_diffs.append(diff)
-            # only add one click per hitobj
-            if len(temp_diffs) != 0:
-                index = temp_diffs.index(min(temp_diffs, key=abs))
-                diff_array.append(temp_diffs[index])
-
+        
+        for hit, press in filtered_array:
+            diff_array.append(press[0]-hit[0])
         return np.std(diff_array) * 10
 
     def _parse_beatmap(self, beatmap):
@@ -42,9 +28,8 @@ class Investigator:
         # parse hitobj
         for hit in beatmap.hitobjects:
             if not 8 & hit.type:
-                hitobjs.append(hit.time)
+                hitobjs.append([hit.time, hit.x, hit.y])
         return hitobjs
-
 
     def _parse_keys(self, replay):
         keypresses = []
@@ -53,8 +38,29 @@ class Investigator:
             if keypress[3] == 5 or keypress[3] == 10:
                 if not flip:
                     flip = True
-                    keypresses.append(keypress[0])
+                    keypresses.append(keypress[:3])  # t,x,y
             else:
                 if flip:
                     flip = False
         return keypresses
+
+
+    def _filter_hits(self, hitobjs, keypresses):
+        array = []
+        hitwindow = 150 + 50 * (5 - self.beatmap.difficulty["OverallDifficulty"]) / 5
+        # inefficient as fugg, but not important rn
+        # filters out all clicks that didn't hit anything and stores hit and hitobj to array
+        for hitobj in hitobjs:
+            temp_diffs = []
+            temp_hits = []
+            for press in keypresses:
+                if hitobj[0] > press[0]-(hitwindow/2) and hitobj[0] < press[0]+(hitwindow/2):
+                    diff = press[0]-hitobj[0]
+                    temp_diffs.append(diff)
+                    temp_hits.append(press)
+            # only add one click per hitobj
+            if len(temp_diffs) != 0:
+                index = temp_diffs.index(min(temp_diffs, key=abs))
+                array.append([hitobj, temp_hits[index]])
+        print(len(array))
+        return array
