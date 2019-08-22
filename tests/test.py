9b52a748af99686a44416a2c4e0e8b38279c2ab6
Liam DeVoe <orionldevoe@gmail.com>
2019-08-22 15:05:07 -0400

    add options test cases

--- tests/test.py
+++ tests/test.py
@@ -1,10 +1,17 @@
 from unittest import TestCase
 from pathlib import Path
 
-from circleguard import Circleguard, Check, ReplayMap, ReplayPath, Detect, RatelimitWeight
+from circleguard import Circleguard, Check, ReplayMap, ReplayPath, Detect, RatelimitWeight, set_options
 
 KEY = input("Enter your api key: ")
 RES = Path(__file__).parent / "resources"
+set_options(loglevel=20)
+
+def log(function):
+    def wrapper(*args, **kwargs):
+        print(f"Running test {function.__name__}")
+        return function(*args, **kwargs)
+    return wrapper
 
 
 class TestReplays(TestCase):
@@ -13,6 +20,7 @@ class TestReplays(TestCase):
     def setUpClass(cls):
         cls.cg = Circleguard(KEY)
 
+    @log
     def test_cheated_replay(self):
         # taken from http://redd.it/bvfv8j, remodded replay by same user (CielXDLP) from HDHR to FLHDHR
         replays = [ReplayPath(RES / "stolen_replay1.osr"), ReplayPath(RES / "stolen_replay2.osr")]
@@ -36,6 +44,7 @@ class TestReplays(TestCase):
         self.assertEqual(later.replay_id, 2805164683, "Later replay id was not correct")
         self.assertEqual(r1.username, r2.username, "Replay usernames did not match")
 
+    @log
     def test_legitimate_replay(self):
         replays = [ReplayPath(RES / "legit_replay1.osr"), ReplayPath(RES / "legit_replay2.osr")]
         c = Check(replays, detect=Detect.STEAL)
@@ -59,6 +68,7 @@ class TestReplays(TestCase):
         self.assertEqual(earlier.username, "Crissinop", "Earlier username was not correct")
         self.assertEqual(later.username, "TemaZpro", "Later username was not correct")
 
+    @log
     def test_loading_replaypath(self):
         r = ReplayPath(RES / "example_replay.osr")
         self.assertFalse(r.loaded, "Loaded status was not correct")
@@ -70,6 +80,7 @@ class TestReplays(TestCase):
         self.assertEqual(r.weight, RatelimitWeight.LIGHT, "RatelimitWeight was not correct")
         self.assertTrue(r.loaded, "Loaded status was not correct")
 
+    @log
     def test_loading_replaymap(self):
         # Toy HDHR score on Pretender
         r = ReplayMap(221777, 2757689)
@@ -84,10 +95,95 @@ class TestReplays(TestCase):
         self.assertTrue(r.loaded, "Loaded status was not correct")
 
 
-class TestRelax(TestCase):
 
-    def test_cheated_replay(self):
-        ...
+class TestOption(TestCase):
 
-    def test_legitimate_replay(self):
-        ...
+    @classmethod
+    def setUpClass(cls):
+        cls.cg = Circleguard(KEY)
+        cls.r1 = ReplayMap(221777, 2757689)
+        cls.r2 = ReplayMap(221777, 3219026)
+        cls.r3 = ReplayMap(221777, 3256299)
+        cls.cg.load(cls.r1)
+        cls.cg.load(cls.r2)
+        cls.cg.load(cls.r3)
+
+        # r1 <-> r2 similarity: 20.875089740385583
+        # r2 <-> r3 similarity:
+        # r3 <-> r1 similarity:
+
+    def setUp(self):
+        # reset settings so methods don't interfere with each other's settings
+        self.cg = Circleguard(KEY)
+
+    @log
+    def test_steal_thresh_check_true(self):
+        set_options(steal_thresh=19)
+        self.cg.set_options(steal_thresh=19)
+        c = Check([self.r1, self.r2], steal_thresh=21)
+        r = list(self.cg.run(c))[0]
+        self.assertTrue(r.ischeat, "Thresh should have been set to detect a cheat at the Check level but was not")
+
+    @log
+    def test_steal_thresh_check_false(self):
+        set_options(steal_thresh=21)
+        self.cg.set_options(steal_thresh=21)
+        c = Check([self.r1, self.r2], steal_thresh=19)
+        r = list(self.cg.run(c))[0]
+        self.assertFalse(r.ischeat, "Thresh should have been set to miss a cheat at the Check level but was not")
+
+    @log
+    def test_steal_thresh_check_without_class_true(self):
+        set_options(steal_thresh=19)
+        c = Check([self.r1, self.r2], steal_thresh=21)
+        r = list(self.cg.run(c))[0]
+        self.assertTrue(r.ischeat, "Thresh should have been set to detect a cheat at the Check level but was not")
+
+    @log
+    def test_steal_thresh_check_without_class_false(self):
+        set_options(steal_thresh=21)
+        c = Check([self.r1, self.r2], steal_thresh=19)
+        r = list(self.cg.run(c))[0]
+        self.assertFalse(r.ischeat, "Thresh should have been set to miss a cheat at the Check level but was not")
+
+    @log
+    def test_steal_thresh_check_without_class_or_global_true(self):
+        c = Check([self.r1, self.r2], steal_thresh=21)
+        r = list(self.cg.run(c))[0]
+        self.assertTrue(r.ischeat, "Thresh should have been set to detect a cheat at the Check level but was not")
+
+    def test_steal_thresh_check_without_class_or_global_false(self):
+        c = Check([self.r1, self.r2], steal_thresh=19)
+        r = list(self.cg.run(c))[0]
+        self.assertFalse(r.ischeat, "Thresh should have been set to miss a cheat at the Check level but was not")
+
+
+    @log
+    def test_steal_thresh_class_true(self):
+        set_options(steal_thresh=19)
+        self.cg.set_options(steal_thresh=21)
+        c = Check([self.r1, self.r2])
+        r = list(self.cg.run(c))[0]
+        self.assertTrue(r.ischeat, "Thresh should have been set to detect a cheat at the class level but was not")
+
+    @log
+    def test_steal_thresh_class_false(self):
+        set_options(steal_thresh=21)
+        self.cg.set_options(steal_thresh=19)
+        c = Check([self.r1, self.r2])
+        r = list(self.cg.run(c))[0]
+        self.assertFalse(r.ischeat, "Thresh should have been set to miss a cheat at the class level but was not")
+
+    @log
+    def test_steal_thresh_class_without_global_true(self):
+        self.cg.set_options(steal_thresh=21)
+        c = Check([self.r1, self.r2])
+        r = list(self.cg.run(c))[0]
+        self.assertTrue(r.ischeat, "Thresh should have been set to detect a cheat at the class level but was not")
+
+    @log
+    def test_steal_thresh_class_without_global_false(self):
+        self.cg.set_options(steal_thresh=19)
+        c = Check([self.r1, self.r2])
+        r = list(self.cg.run(c))[0]
+        self.assertFalse(r.ischeat, "Thresh should have been set to miss a cheat at the class level but was not")
