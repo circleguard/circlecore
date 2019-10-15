914883f7a0c07a0780448e0b674f6f8405067c3e
InvisibleSymbol <pettypalkovicsreal@gmail.com>
2019-10-15 20:51:49 +0200

    rewrite class to allow addition & subtraction of Mods
    Co-Authored-By: Liam DeVoe <tybug@users.noreply.github.com>

--- circleguard/enums.py
+++ circleguard/enums.py
@@ -15,59 +15,143 @@ class Error(Enum):
                                                                 "to the developers at https://github.com/circleguard/circlecore"]
 
 int_to_mod = {
-    0           : ["NoMod",         "NM"],
-    1           : ["NoFail",        "NF"],
-    2           : ["Easy",          "EZ"],
-    4           : ["NoVideo",       "NV"],
-    8           : ["Hidden",        "HD"],
-    16          : ["HardRock",      "HR"],
-    32          : ["SuddenDeath",   "SD"],
-    64          : ["DoubleTime",    "DT"],
-    128         : ["Relax",         "RL"],
-    256         : ["HalfTime",      "HT"],
-    512         : ["Nightcore",     "NC"],
-    1024        : ["Flashlight",    "FL"],
-    2048        : ["Autoplay",      "CN"],
-    4096        : ["SpunOut",       "SO"],
-    8192        : ["Autopilot",     "AP"],
-    16384       : ["Perfect",       "PF"],
-    32768       : ["Key4",          "K4"],
-    65536       : ["Key5",          "K5"],
-    131072      : ["Key6",          "K6"],
-    262144      : ["Key7",          "K7"],
-    524288      : ["Key8",          "K8"],
-    1015808     : ["keyMod",        "KM"],
-    1048576     : ["FadeIn",        "FI"],
-    2097152     : ["Random",        "RD"],
-    4194304     : ["LastMod",       "LM"],
-    8388608     : ["TargetPractice","TP"],
-    16777216    : ["Key9",          "K9"],
-    33554432    : ["Coop",          "CO"],
-    67108864    : ["Key1",          "K1"],
-    134217728   : ["Key3",          "K3"],
-    268435456   : ["Key2",          "K2"]
+    0           : ["NM",         "NoMod"],
+    1           : ["NF",        "NoFail"],
+    2           : ["EZ",          "Easy"],
+    4           : ["NV",       "NoVideo"],
+    8           : ["HD",        "Hidden"],
+    16          : ["HR",      "HardRock"],
+    32          : ["SD",   "SuddenDeath"],
+    64          : ["DT",    "DoubleTime"],
+    128         : ["RL",         "Relax"],
+    256         : ["HT",      "HalfTime"],
+    512         : ["NC",     "Nightcore"],
+    1024        : ["FL",    "Flashlight"],
+    2048        : ["CN",      "Autoplay"],
+    4096        : ["SO",       "SpunOut"],
+    8192        : ["AP",     "Autopilot"],
+    16384       : ["PF",       "Perfect"],
+    32768       : ["K4",          "Key4"],
+    65536       : ["K5",          "Key5"],
+    131072      : ["K6",          "Key6"],
+    262144      : ["K7",          "Key7"],
+    524288      : ["K8",          "Key8"],
+    1015808     : ["KM",        "keyMod"],
+    1048576     : ["FI",        "FadeIn"],
+    2097152     : ["RD",        "Random"],
+    4194304     : ["LM",       "LastMod"],
+    8388608     : ["TP","TargetPractice"],
+    16777216    : ["K9",          "Key9"],
+    33554432    : ["CO",          "Coop"],
+    67108864    : ["K1",          "Key1"],
+    134217728   : ["K3",          "Key3"],
+    268435456   : ["K2",          "Key2"]
 }
-        
-class Mod():
-    def __init__(self, input_enum):
-        if isinstance(input_enum, str):
-            self.value = [k for k, v in int_to_mod.items() if input_enum in v][0]
+
+class ModCombination():
+    """
+    An ingame osu! mod, or combination of mods.
+
+    Notes
+    -----
+    This class is not meant to be instantiated. Use :class:`~.Mod` and combine
+    them as necessary instead.
+
+    A full list of mods and their specification can be found at
+    https://osu.ppy.sh/help/wiki/Game_Modifiers.
+    """
+
+    def __init__(self, value):
+        self.value = value
+        # avoid infinite recursion with every mod decomposing into itself
+        # ad infinitum
+        if self.value in int_to_mod:
+            self.short_name = int_to_mod[value][0]
+            self.long_name = int_to_mod[value][1]
         else:
-            self.value = input_enum
-        current_array = int_to_mod[self.value]
-        self.name = current_array[0]
-        self.short_name = current_array[1]
+            self.short_name = "".join(int_to_mod[mod.value][0] for mod in self.decompose())
+            self.long_name = " ".join(int_to_mod[mod.value][1] for mod in self.decompose())
 
     def __eq__(self, other):
-        """Override the default Equals behavior"""
+        """Compares the ``value`` of each object"""
         return self.value == other.value
 
     def __ne__(self, other):
-        """Override the default Unequal behavior"""
+        """Compares the ``value`` of each object"""
         return self.value != other.value
-    
+
+    def __add__(self, other):
+        """Returns a Mod representing the bitwise OR of the two Mods"""
+        return ModCombination(self.value | other.value)
+
+    def __sub__(self, other):
+        return ModCombination(self.value ^ other.value)
+
     def __hash__(self):
         return self.value
+
+    def __repr__(self):
+        return self.__str__()
+
+    def __str__(self):
+        return " + ".join("Mod." + mod.short_name for mod in self.decompose())
+
+    def __contains__(self, other):
+        return bool(self.value & other.value)
+
+    def decompose(self):
+        """
+        Decomposes this mod into its base component mods, which are
+        :class:`~.ModCombination`\s with a ``value`` of a power of two.
+
+        Returns
+        -------
+        list: :class:`~.ModCombination`
+            A list of the component :class:`~.ModCombination`\s of this mod.
+        """
+        return [ModCombination(mod) for mod in int_to_mod.keys() if self.value & mod]
+
+class Mod():
+    """
+    An ingame osu! mod.
+
+    Notes
+    -----
+    A full list of mods and their specification can be found at
+    https://osu.ppy.sh/help/wiki/Game_Modifiers.
+    """
+
+    NM = NoMod          =  ModCombination(0)
+    NF = NoFail         =  ModCombination(1)
+    EZ = Easy           =  ModCombination(2)
+    NV = NoVideo        =  ModCombination(4)
+    HD = Hidden         =  ModCombination(8)
+    HR = HardRock       =  ModCombination(16)
+    SD = SuddenDeath    =  ModCombination(32)
+    DT = DoubleTime     =  ModCombination(64)
+    RL = Relax          =  ModCombination(128)
+    HT = HalfTime       =  ModCombination(256)
+    NC = Nightcore      =  ModCombination(512)
+    FL = Flashlight     =  ModCombination(1024)
+    CN = Autoplay       =  ModCombination(2048)
+    SO = SpunOut        =  ModCombination(4096)
+    AP = Autopilot      =  ModCombination(8192)
+    PF = Perfect        =  ModCombination(16384)
+    K4 = Key4           =  ModCombination(32768)
+    K5 = Key5           =  ModCombination(65536)
+    K6 = Key6           =  ModCombination(131072)
+    K7 = Key7           =  ModCombination(262144)
+    K8 = Key8           =  ModCombination(524288)
+    KM = keyMod         =  ModCombination(1015808)
+    FI = FadeIn         =  ModCombination(1048576)
+    RD = Random         =  ModCombination(2097152)
+    LM = LastMod        =  ModCombination(4194304)
+    TP = TargetPractice =  ModCombination(8388608)
+    K9 = Key9           =  ModCombination(16777216)
+    CO = Coop           =  ModCombination(33554432)
+    K1 = Key1           =  ModCombination(67108864)
+    K3 = Key3           =  ModCombination(134217728)
+    K2 = Key2           =  ModCombination(268435456)
 
 class Detect(Flag):
                    # (in binary)
