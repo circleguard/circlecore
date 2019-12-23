7ecbf458b7266672ae66d270192dfe3561b218d2
InvisibleSymbol <pettypalkovicsreal@gmail.com>
2019-12-23 22:22:08 +0100

    add more tests

--- tests/test.py
+++ tests/test.py
@@ -230,6 +230,14 @@ class TestLoader(CGTestCase):
         result = self.loader.user_id("727")
         self.assertEqual(result, 10750899)
 
+    def test_wrong_username(self):
+        result = self.loader.username(0)
+        self.assertEqual(result, "")
+
+    def test_working_username(self):
+        result = self.loader.username(13506780)
+        self.assertEqual(result, "] [")
+
 
 if __name__ == '__main__':
     suite = TestSuite()
