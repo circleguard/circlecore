ef696a327eaccd1e7e9b88bd427ea19b828c99e0
InvisibleSymbol <pettypalkovicsreal@gmail.com>
2019-12-24 21:51:18 +0100

    clean up tests

--- tests/test.py
+++ tests/test.py
@@ -210,36 +210,32 @@ class TestLoader(CGTestCase):
     def setUpClass(cls):
         cls.loader = Loader(KEY)
 
-    def test_incorrect_map_id(self):
+    def test_loading_map_id(self):
         result = self.loader.map_id("E")
         self.assertEqual(result, 0)
 
-    def test_working_map_id(self):
         result = self.loader.map_id("9d0a8fec2fe3f778334df6bdc60b113c")
         self.assertEqual(result, 221777)
 
-    def test_incorrect_user_id(self):
+    def test_loading_user_id(self):
         result = self.loader.user_id("E")
         self.assertEqual(result, 0)
 
-    def test_working_user_id1(self):
         result = self.loader.user_id("] [")
         self.assertEqual(result, 13506780)
 
-    def test_working_user_id2(self):
         result = self.loader.user_id("727")
         self.assertEqual(result, 10750899)
 
-    def test_incorrect_username(self):
+    def test_loading_username(self):
         result = self.loader.username(0)
         self.assertEqual(result, "")
 
-    def test_working_username(self):
         result = self.loader.username(13506780)
         self.assertEqual(result, "] [")
 
     def test_incorrect_key(self):
-        loader = Loader("LULW 727 LULW")
+        loader = Loader("incorrect key")
         self.assertRaises(InvalidKeyException, loader.username, 13506780)
         self.assertRaises(InvalidKeyException, loader.user_id, "] [")
         self.assertRaises(InvalidKeyException, loader.map_id, "9d0a8fec2fe3f778334df6bdc60b113c")
