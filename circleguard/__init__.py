a672ee92c14c968f3e5594da06157b07ecf34bb4
Liam DeVoe <orionldevoe@gmail.com>
2020-04-20 17:32:40 -0400

    tidy up __init__

--- circleguard/__init__.py
+++ circleguard/__init__.py
@@ -2,8 +2,7 @@ import logging
 
 from circleguard.circleguard import Circleguard, set_options
 from circleguard.loadable import Check, Replay, ReplayMap, ReplayPath, Map, User, MapUser, ReplayContainer, LoadableContainer, Loadable
-from circleguard.enums import Key, Mod, RatelimitWeight
-from circleguard.detect import Detect, StealDetect, RelaxDetect, CorrectionDetect
+from circleguard.enums import Key, Mod, RatelimitWeight, Detect, ResultType
 from circleguard.utils import TRACE, ColoredFormatter
 from circleguard.loader import Loader
 from circleguard.replay_info import ReplayInfo
@@ -12,7 +11,7 @@ from circleguard.exceptions import (CircleguardException, InvalidArgumentsExcept
         InvalidJSONException, ReplayUnavailableException)
 from circleguard.version import __version__
 from circleguard.result import (Result, InvestigationResult, ComparisonResult,
-        RelaxResult, ResultType, CorrectionResult)
+        StealResult, RelaxResult, CorrectionResult)
 from circleguard.span import Span
 
 logging.addLevelName(TRACE, "TRACE")
@@ -21,11 +20,30 @@ handler_stream = logging.StreamHandler()
 handler_stream.setFormatter(formatter)
 logging.getLogger("circleguard").addHandler(handler_stream)
 
-__all__ = ["Circleguard", "set_options", "Check", "Replay", "ReplayMap", "StealDetect", "RelaxDetect",
-           "CorrectionDetect", "ReplayPath", "Detect", "TRACE", "ColoredFormatter", "Loader", "ReplayInfo",
-           "__version__", "RatelimitWeight", "Result", "InvestigationResult",
-           "ComparisonResult", "RelaxResult", "ResultType",
-           "CircleguardException", "InvalidArgumentsException", "Map", "User",
-           "APIException", "NoInfoAvailableException", "UnknownAPIException", "InternalAPIException",
-           "InvalidKeyException", "RatelimitException", "InvalidJSONException", "ReplayUnavailableException", "Key",
-           "Mod", "CorrectionResult", "MapUser", "ReplayContainer", "LoadableContainer", "Loadable", "Span"]
+__all__ = [
+# core
+"Circleguard", "set_options",
+# loadables
+"Check", "ReplayContainer", "LoadableContainer", "Map", "User", "MapUser",
+"Replay", "ReplayMap", "ReplayPath", "Loadable",
+# enums
+"Key", "Mod", "RatelimitWeight", "Detect", "ResultType",
+# utils
+"TRACE",
+# loader
+"Loader",
+# replay info
+"ReplayInfo",
+# exceptions
+"CircleguardException", "InvalidArgumentsException", "APIException",
+"NoInfoAvailableException", "UnknownAPIException", "InternalAPIException",
+"InvalidKeyException", "RatelimitException", "InvalidJSONException",
+"ReplayUnavailableException",
+# version
+"__version__",
+# results
+"Result", "InvestigationResult", "ComparisonResult", "StealResult",
+"RelaxResult", "CorrectionResult",
+# span
+"Span"
+]
