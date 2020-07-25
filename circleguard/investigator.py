0e24dae75ff7f0ea6d9445a69723b8f980339992
Liam DeVoe <orionldevoe@gmail.com>
2020-07-24 23:41:55 -0400

    add changelog link for sliderbug

--- circleguard/investigator.py
+++ circleguard/investigator.py
@@ -28,6 +28,7 @@ class Investigator:
     """
 
     MASK = int(Key.M1) | int(Key.M2)
+    # https://osu.ppy.sh/home/changelog/stable40/20190513.2
     VERSION_SLIDERBUG_FIXED = 20190513
 
     def __init__(self, replay, detect, max_angle, min_distance, beatmap=None):
