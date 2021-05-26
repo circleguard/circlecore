74b192cae9dda133465514e9639f96ebc993b5ee
Liam DeVoe <orionldevoe@gmail.com>
2021-05-25 23:40:10 -0400

    require osrparse 5.0.0

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     license = "MIT",
     packages=find_packages(),
     install_requires=[
-        "osrparse~=4.0.0",
+        "osrparse~=5.0.0",
         "ossapi==1.3.0",
         "wtc==1.2.1",
         "numpy",
