8fdc0474f5ea8b8e3170b234e4134fd084cc2ce2
Liam DeVoe <orionldevoe@gmail.com>
2022-04-29 00:23:31 -0400

    reword outliers docs

--- circleguard/utils.py
+++ circleguard/utils.py
@@ -221,7 +221,7 @@ def hitradius(CS):
 
 def filter_outliers(arr, bias=1.5):
     """
-    Filters outliers from ``arr``.
+    Returns ``arr` with outliers removed.
 
     Parameters
     ----------
